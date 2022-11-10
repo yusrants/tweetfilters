3dac94f849258e8619ada2ad148f9741dbc5e7c0
yusrants <yusrants@users.noreply.github.com>
2022-11-10 17:46:22 +0500

    Add filter functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
 
-export default App;
+  constructor(){  
+    super();  
+    // assuming we have a list of tweets containing info about companies.
+    this.state = {
+      tweets: [{desc: "Hello, Im a tweet about Pakistan"},
+      {desc: "Hello, Im a tweet about USA"},{desc: "Hello, Im a tweet about Italy"},
+      {desc: "Hello, Im a tweet about Spain"},{desc: "Hello, Im a tweet about Turkey"},
+      {desc: "News about pakistan"},{desc: "This is news about UAE"}],
+
+      country: "",
+      filteredTweets: [],
+    }
+    }
+
+  handleChange = (event) => {
+      this.setState({country: event.target.value});
+      this.setState({filteredTweets : []})
+    }
+
+  filterTweets = (e) => {
+    e.preventDefault();
+    const filtered = this.state.tweets.filter((tweet)=>tweet.desc.toLowerCase().indexOf(this.state.country.toLowerCase().replace(/ /g,'')
+    ) > -1);
+    this.setState({filteredTweets : filtered})
+  }
+
+  render() {
+    return <div className='App-header'>
+
+        <form >
+          <label>
+          <h3> Filter Tweets: </h3>
+            <input type="text"  value={this.state.country} onChange={(e) => this.handleChange(e)} />
+          </label>
+          <button type="submit" onClick={this.filterTweets}> Filter</button>
+        </form>
+
+        <div className='container'>
+          <h2> List of available tweets</h2>
+          {this.state.filteredTweets && this.state.filteredTweets.length > 0 ? this.state.filteredTweets.map((tweet)=>{
+            return <div className='tweet'> <h4> {tweet.desc}</h4></div>}): this.state.tweets.map((tweet)=>{
+              return <div className='tweet'> <h4> {tweet.desc}</h4></div>})}
+        </div>
+    </div>;
+  }
+}
